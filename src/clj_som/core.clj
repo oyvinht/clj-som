242c21656f31f8906dd78e1411ac364bcad41b91
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2021-05-29 18:05:35 +0200

    Add improved-vec and train functions for SOM

--- src/clj_som/core.clj
+++ src/clj_som/core.clj
@@ -48,3 +48,59 @@
       [[0 0] (diff-fn input-vec (aget nodes 0 0))] ; 0,0 Is best at start
       (range 1 (alength nodes))))))
 
+(defn- improved-vec [target-vec train-vec learning-rate distance-factor]
+  "Create a new target-vec closer to train-vec."
+  (let [v (make-array Double/TYPE (alength ^"[D" target-vec))]
+    (doall (for [i (range (alength ^"[D" target-vec))]
+             (aset ^"[D" v i (+ (aget ^"[D" target-vec i)
+                                (* distance-factor
+                                   learning-rate
+                                   (- (aget ^"[D" train-vec i)
+                                      (aget ^"[D" target-vec i)))))))
+    v))
+  
+(defn train [som sample-generator num-iterations]
+  "Train SOM."
+  (let [new-nodes (aclone  ^"[[Ljava.lang.Object;" (:nodes som))
+        x-size (alength new-nodes)
+        y-size (alength ^"[[Ljava.lang.Object;" (aget new-nodes 0))
+        map-radius (/ (max x-size y-size) 2)
+        time-constant (/ num-iterations (Math/log map-radius))
+        calc-radius (fn [iteration]
+                      (* map-radius
+                         (Math/exp (- (/ iteration time-constant)))))
+        calc-learning-rate (fn [iteration]
+                             (* 0.1 ; TODO: start-learn-rate
+                                (Math/exp (- (/ iteration num-iterations)))))]
+    (assoc
+     som :nodes
+     (reduce
+      (fn [nodes iteration]
+        (let [radius (calc-radius iteration)
+              sample-vec (sample-generator)
+              bmu (find-bmu som sample-vec euclidean-dist)]
+          ;; TODO: Don't go through everything - only neighborhood
+          (reduce
+           (fn [nodes r-idx]
+             (reduce
+              (fn [nodes c-idx]
+                (let [distance (Math/sqrt
+                                (+ (Math/pow (- r-idx (first bmu)) 2)
+                                   (Math/pow (- c-idx (second bmu)) 2)))]
+                  (when (< distance radius)
+                    (aset nodes r-idx c-idx
+                          (improved-vec (aget nodes r-idx c-idx)
+                                        sample-vec
+                                        (calc-learning-rate iteration)
+                                        (Math/exp (- ; Distance factor
+                                                   (/
+                                                    (Math/pow distance 2)
+                                                    (* radius 2)))))))
+                  nodes))
+              nodes
+              (range y-size)))
+           nodes
+           (range x-size))))
+      new-nodes
+      (range num-iterations)))))
+
