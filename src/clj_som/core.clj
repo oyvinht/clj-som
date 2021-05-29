ef7bf3a4a49224cb93fb2a134bfc5aa5546a5290
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2021-05-29 19:51:14 +0200

    Add UMatrix generator

--- src/clj_som/core.clj
+++ src/clj_som/core.clj
@@ -59,7 +59,7 @@
                                       (aget ^"[D" target-vec i)))))))
     v))
   
-(defn train [som sample-generator num-iterations]
+(defn train [som sample-generator num-iterations dist-fn]
   "Train SOM."
   (let [new-nodes (aclone  ^"[[Ljava.lang.Object;" (:nodes som))
         x-size (alength new-nodes)
@@ -78,7 +78,7 @@
       (fn [nodes iteration]
         (let [radius (calc-radius iteration)
               sample-vec (sample-generator)
-              bmu (find-bmu som sample-vec euclidean-dist)]
+              bmu (find-bmu som sample-vec dist-fn)]
           ;; TODO: Don't go through everything - only neighborhood
           (reduce
            (fn [nodes r-idx]
@@ -104,3 +104,149 @@
       new-nodes
       (range num-iterations)))))
 
+(defn u-matrix [som dist-fn]
+  "Create Unified distance Matrix for SOM."
+  (let [nodes ^"[Ljava.lang.Object;" (:nodes som)
+        x-size (- (* (alength nodes) 2) 1)
+        y-size (- (* (alength ^"[Ljava.lang.Object;" (aget nodes 0)) 2) 1)
+        umatrix (make-array Double/TYPE x-size y-size)]
+    ;; First pass: Fill in all distances between nodes
+    (reduce
+     (fn [res y]
+       (reduce
+        (fn [res x]
+          (if (even? y) ; Even y: Dist between left and right of pos
+            (let [lx (/ (dec x) 2) rx (inc lx)]
+              (aset umatrix x y (dist-fn (aget nodes lx (/ y 2))
+                                         (aget nodes rx (/ y 2)))))
+            (if (even? x)
+              ;; Odd y & even x: Dist between over and under pos
+              (let [ux (/ x 2) uy (/ (dec y) 2) ox ux oy (inc uy)]
+                (aset umatrix x y (dist-fn (aget nodes ox oy)
+                                           (aget nodes ux uy))))
+              ;; Odd y & odd x: Mean of diagonals
+              (let [ulx (/ (dec x) 2) luy (/ (dec y) 2) ; Under-left
+                    orx (inc ulx) ory (inc luy) ; Over-right
+                    urx orx ury luy ; Under-right
+                    olx ulx oly ory] ; Over-left
+                ;;(aset umatrix x y (aget umatrix x y))
+                 (aset umatrix x y (/ (+ (dist-fn (aget nodes ulx luy)
+                                                  (aget nodes orx ory))
+                                         (dist-fn (aget nodes olx oly)
+                                                  (aget nodes urx ury)))
+                                      2))))))
+        nil
+        (if (even? y)
+          (range 1 x-size 2) ; Skip slots with nodes in
+          (range x-size))))
+     nil
+     (range y-size))
+    ;; Second pass: Fill in averages at node positions
+    (reduce
+      (fn [res y]
+        (reduce
+         (fn [res x]
+           (aset umatrix x y
+                 (cond (= y 0)
+                       (cond (= x 0) ; Lower left
+                             (/ (+ (aget umatrix 0 1)
+                                   (aget umatrix 1 1)
+                                   (aget umatrix 1 0))
+                                3)
+                             (= x (dec x-size)) ; Lower right
+                             (/ (+ (aget umatrix (dec x) y)
+                                   (aget umatrix (dec x) (inc y))
+                                   (aget umatrix x (inc y)))
+                                3)
+                             true
+                             (/ (+ (aget umatrix (dec x) y)
+                                   (aget umatrix (dec x) (inc y))
+                                   (aget umatrix x (inc y))
+                                   (aget umatrix (inc x) (inc y))
+                                   (aget umatrix (inc x) y))
+                                5))
+                       (= y (dec y-size))
+                       (cond (= x 0) ; Upper left
+                             (/ (+ (aget umatrix 0 (dec y))
+                                   (aget umatrix 1 (dec y))
+                                   (aget umatrix 1 y))
+                                3)
+                             (= x (dec x-size)) ; Upper right
+                             (/ (+ (aget umatrix (dec x) y)
+                                   (aget umatrix (dec x) (dec y))
+                                   (aget umatrix x (dec y)))
+                                3)
+                             true
+                             (/ (+ (aget umatrix (dec x) y) 
+                                   (aget umatrix (dec x) (dec y))
+                                   (aget umatrix x (dec y))
+                                   (aget umatrix (inc x) (dec y))
+                                   (aget umatrix (inc x) y))
+                                5))
+                       true
+                       (cond (= x 0) ; Middle left
+                             (/ (+ (aget umatrix x (inc y))
+                                   (aget umatrix (inc x) (inc y))
+                                   (aget umatrix (inc x) y)
+                                   (aget umatrix (inc x) (dec y))
+                                   (aget umatrix x (dec y)))
+                                5)
+                             (= x (dec x-size)) ; Middle right
+                             (/ (+ (aget umatrix x (inc y))
+                                   (aget umatrix (dec x) (inc y))
+                                   (aget umatrix (dec x) y)
+                                   (aget umatrix (dec x) (dec y))
+                                   (aget umatrix x (dec y)))
+                                5)
+                             true
+                             (/ (+ (aget umatrix x (inc y))
+                                   (aget umatrix (inc x) (inc y))
+                                   (aget umatrix (inc x) y)
+                                   (aget umatrix (inc x) (dec y))
+                                   (aget umatrix x (dec y))
+                                   (aget umatrix (dec x) (dec y))
+                                   (aget umatrix (dec x) y)
+                                   (aget umatrix (dec x) (inc y)))
+                                8)))))
+         res
+         (range 0 x-size 2)))
+      umatrix
+      (range 0 y-size 2))
+    ;; Third pass: Normalize to range 0:1
+    (let [smallest (reduce (fn [smallest y]
+                             (min smallest
+                                  (reduce
+                                   (fn [smallest x]
+                                     (min smallest (aget umatrix x y)))
+                                   smallest
+                                   (range x-size))))
+                           (aget umatrix 0 0)
+                           (range y-size))]
+      ;; Move all down so that min is at 0
+      (doall (for [x (range x-size) y (range y-size)]
+               (aset umatrix x y (- (aget umatrix x y) smallest))))
+      ;; Squash into 0:1 range
+      (let [biggest (reduce (fn [smallest y]
+                             (max smallest
+                                  (reduce
+                                   (fn [smallest x]
+                                     (max smallest (aget umatrix x y)))
+                                   smallest
+                                   (range x-size))))
+                            (aget umatrix 0 0)
+                            (range y-size))]
+        (doall (for [x (range x-size) y (range y-size)]
+                 (aset umatrix x y (/ (aget umatrix x y) biggest))))
+        umatrix))))
+
+(defn pprint-u-matrix [^"[[D" u-matrix]
+  (reduce (fn [r y] ;; Note: Rotate -90 and flip to show x/y correctly
+            (reduce (fn [r x]
+                      (print (format " %5f" (aget u-matrix x y)))
+                      r)
+                    r
+                    (range (alength u-matrix)))
+            (print "\n"))
+          u-matrix
+          (reverse (range (alength ^"[D" (aget u-matrix 0)))))
+  nil)
--- test/clj_som/core_test.clj
+++ test/clj_som/core_test.clj
@@ -16,7 +16,8 @@
 
 (deftest test-find-bmu
   (testing "Testing find-bmu function."
-    (let [a (to-array-2d [(make-array Double/TYPE 2) (make-array Double/TYPE 2)])]
+    (let [a (to-array-2d [(make-array Double/TYPE 2)
+                          (make-array Double/TYPE 2)])]
       (aset a 0 0 (into-array Double/TYPE [0.5 0.1]))
       (aset a 0 1 (into-array Double/TYPE [0.6 0.1]))
       (aset a 1 0 (into-array Double/TYPE [0.5 0.2]))
@@ -25,3 +26,22 @@
            (#'som.core/find-bmu {:nodes a}
                                 (into-array Double/TYPE [0.49 0.21])
                                 euclidean-dist))))))
+
+;; (deftest test-u-matrix
+;;   (testing "Testing if U-Matrix gets created properly."
+;;     (let [a (to-array-2d [(make-array Double/TYPE 3)
+;;                           (make-array Double/TYPE 3)
+;;                           (make-array Double/TYPE 3)])]
+;;       (aset a 0 0 (into-array Double/TYPE [1.0 2.0]))
+;;       (aset a 0 1 (into-array Double/TYPE [1.0 2.0]))
+;;       (aset a 0 2 (into-array Double/TYPE [1.0 1.0]))
+;;       (aset a 1 0 (into-array Double/TYPE [1.0 1.0]))
+;;       (aset a 1 1 (into-array Double/TYPE [1.0 1.0]))
+;;       (aset a 1 2 (into-array Double/TYPE [1.0 1.0]))
+;;       (aset a 2 0 (into-array Double/TYPE [1.0 3.0]))
+;;       (aset a 2 1 (into-array Double/TYPE [1.0 1.0]))
+;;       (aset a 2 2 (into-array Double/TYPE [1.0 0.0]))
+;;       ;;
+;;       (let [um (u-matrix {:nodes a} euclidean-dist)]
+;;         (=
+       
