5b94791611fa2dd8273aa2f7e8faa32d73be2511
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2021-06-03 13:34:39 +0200

    Add profiler dependencies.

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "https://github.com/oyvinht/clj-som"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[com.clojure-goes-fast/clj-async-profiler "0.5.0"]
+                 [criterium "0.4.6"]
+                 [org.clojure/clojure "1.10.1"]]
+  :jvm-opts ["-Djdk.attach.allowAttachSelf"]
   :repl-options {:init-ns som.core})
